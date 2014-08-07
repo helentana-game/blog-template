1da2a95f74f2bc780d4a6b7b2a36681dd638e5a3
speed_galosh <gmariczzz@gmail.com>
2014-08-07 20:41:22 +0400

    Добавлены index.php и style.css

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <title>Возрождение Элентаны - Браузерная РПГ</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="wrapper">
+    <header class="header">
+        <h1>Браузерная РПГ &quot;Возрождение Элентаны&quot;</h1>
+    </header><!-- .header -->
+
+    <div class="middle">
+        
+        <div class="container">
+            <main class="content">
+                История началась!
+            </main><!-- .content -->
+        </div><!-- .container -->
+
+        <aside class="right-sidebar">
+            &nbsp;
+        </aside><!-- .right-sidebar -->
+
+    </div><!-- .middle -->
+
+    <footer class="footer">
+        (c) 2014
+    </footer><!-- .footer -->
+
+</div><!-- .wrapper -->
+
+</body>
+</html>
